7a396c88568f2e29bc04729dacf0f01d68e7e594
janelldizon4-debug <janelldizon4@gmail.com>
2025-12-27 22:35:27 +0800

    Update service worker to cache additional files

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,25 @@
 const CACHE_NAME = "cpm-notes-v1";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./service-worker.js"
+const FILES = [
+  "/King/",
+  "/King/index.html",
+  "/King/manifest.json",
+  "/King/service-worker.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
